05052fade7153793acabc6a1aaa246c07ae40b65
Devul <devultj@gmail.com>
2015-09-01 21:21:18 +0100

    Fixed some money issues

--- gamemode/cl_player.lua
+++ gamemode/cl_player.lua
@@ -1,8 +1,6 @@
 local playerData = playerData or {}
 
-local meta = FindMetaTable("Player")
-
-function meta:setMoney( amount )
+local function setMoney( amount )
 	if not amount then amount = 0 end
 	if amount < 1 or amount > GetConVar("fruit_cs_maxMoney"):GetInt() then return end
 
@@ -10,16 +8,16 @@ function meta:setMoney( amount )
 	playerData.money = amount
 end
 
-function meta:getMoney( amount )
+local function getMoney( amount )
 	return playerData and playerData.money or false
 end
 
 net.Receive("setUpMoney", function(len)
-	LocalPlayer():setMoney()
+	setMoney()
 end)
 
 net.Receive("sendMoneyChange", function(len)
 	local amount = net.ReadInt(16)
 
-	LocalPlayer():setMoney( amount )
+	setMoney( amount )
 end)
