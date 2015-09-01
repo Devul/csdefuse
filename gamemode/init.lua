a5cb20e5440ebabb6d176ce3d98b1e76a9b02367
Devul <devultj@gmail.com>
2015-09-01 02:08:40 +0100

    Old stuff

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -1,29 +1,29 @@
-GRust = GRust or GM
+fruit = fruit or GM
 
 AddCSLuaFile( "cl_init.lua" )
 AddCSLuaFile( "shared.lua" )
 
 include( "shared.lua" )
 
-GRust.Loadout = { "weapon_physcannon" }
+fruit.Loadout = { "weapon_physcannon" }
 
-CreateConVar( "GRust_DebugMode", 1, FCVAR_NOTIFY, "int boolean (0 = false, 1 = true)" )
-GRust.DebugMode = GetConVar( "GRust_DebugMode" )
+CreateConVar( "fruit_DebugMode", 1, FCVAR_NOTIFY, "int boolean (0 = false, 1 = true)" )
+fruit.DebugMode = GetConVar( "fruit_DebugMode" )
 
-function GRust.PrintDebug( any )
-	if tobool( GRust.DebugMode ) then 
+function fruit.PrintDebug( any )
+	if tobool( fruit.DebugMode ) then 
 		return print( any )
 	end
 end
 
-function GRust:PlayerLoadout( client )
-	for _, weapon in pairs( GRust.Loadout ) do
+function fruit:PlayerLoadout( client )
+	for _, weapon in pairs( fruit.Loadout ) do
 		client:Give( weapon )
 	end
 	
-	GRust.PrintDebug( "[DEBUG] "..client:Name().." -> Loadout Assigned" )
+	fruit.PrintDebug( "[DEBUG] "..client:Name().." -> Loadout Assigned" )
 end
 
-function GRust:PlayerInitialSpawn( client )
-		GRust.PrintDebug( "[DEBUG] "..client:Name().." -> Connected" )
+function fruit:PlayerInitialSpawn( client )
+		fruit.PrintDebug( "[DEBUG] "..client:Name().." -> Connected" )
 end
