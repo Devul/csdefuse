a51f253ec5e93b208376897ddd45464f44097b98
Devul <devultj@gmail.com>
2015-09-01 21:47:28 +0100

    add fruit.formatMoney(number)

--- gamemode/cl_player.lua
+++ gamemode/cl_player.lua
@@ -1,6 +1,6 @@
 local playerData = playerData or {}
 
-local function setMoney( amount )
+function setMoney( amount )
 	if not amount then amount = 0 end
 	if amount < 1 or amount > GetConVar("fruit_cs_maxMoney"):GetInt() then return end
 
@@ -8,8 +8,8 @@ local function setMoney( amount )
 	playerData.money = amount
 end
 
-local function getMoney( amount )
-	return playerData and playerData.money or false
+function getMoney( amount )
+	return playerData and playerData.money or 0
 end
 
 net.Receive("setUpMoney", function(len)
@@ -21,3 +21,28 @@ net.Receive("sendMoneyChange", function(len)
 
 	setMoney( amount )
 end)
+
+
+--[[-------------------------------------------------------------------------
+	Turn cash into a nice string.
+---------------------------------------------------------------------------]]
+local function attachMoneyPrefix(str)
+	local config = fruit.config
+	return config.currencyStr and config.currencyStr .. str or str .. config.currencyStr
+end
+
+function fruit.formatMoney(n)
+	if not n then return attachMoneyPrefix("0") end
+
+	if n >= 1e14 then return attachMoneyPrefix(tostring(n)) end
+
+	n = tostring(n)
+	local sep = sep or ","
+	local dp = string.find(n, "%.") or #n+1
+
+	for i=dp-4, 1, -3 do
+		n = n:sub(1, i) .. sep .. n:sub(i+1)
+	end
+
+	return attachMoneyPrefix(n)
+end
