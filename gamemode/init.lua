8107356e570575282d0de339b9e157e20d54da13
Devul <devultj@gmail.com>
2015-09-01 02:52:21 +0100

    Assign first spawn as Spectator, so we can then assign teams

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -10,6 +10,13 @@ fruit.Loadout = { "weapon_physcannon" }
 CreateConVar( "fruit_DebugMode", 1, FCVAR_NOTIFY, "int (0 = false, 1 = true)" )
 fruit.DebugMode = GetConVar( "fruit_DebugMode" )
 
+fruit.isInitialized = false
+
+fruit.inRound = false
+fruit.inWarmup = false
+fruit.inHalfTime = false
+fruit.isFinished = false
+
 function fruit.PrintDebug( any )
 	if tobool( fruit.DebugMode ) then 
 		return print( any )
@@ -26,4 +33,11 @@ end
 
 function fruit:PlayerInitialSpawn( client )
 		fruit.PrintDebug( "[DEBUG] "..client:Name().." -> Connected" )
+end
+
+function fruit:PlayerSpawn( ply )
+	if ply:Team() == 0 then
+		ply:SetTeam( TEAM_SPECTATOR ) 
+		fruit:PlayerSpawnAsSpectator( ply )
+	end
 end
